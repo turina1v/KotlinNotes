b2ab3db450a2e2deb03e967991ef402105d8a6d6
Valentina Tiurina <turina.valentina@gmail.com>
2020-06-10 13:42:26 +0300

    Initial commit

---
+++ app/src/main/java/ru/turina1v/kotlinnotes/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.turina1v.kotlinnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
